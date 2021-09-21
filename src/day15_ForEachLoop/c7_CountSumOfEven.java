f7bc4b3de13b6b8691af42fb80e84a7034a5438f
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-09-20 21:56:51 -0400

    nestedfor

--- src/day15_ForEachLoop/c7_CountSumOfEven.java
+++ src/day15_ForEachLoop/c7_CountSumOfEven.java
@@ -6,12 +6,12 @@ public class c7_CountSumOfEven {
         //from 2d array
         //create a java logic that will print odd and even numbers
         //count of even and odd
-        //also it will print sum of oddnumbers and sum of even numbers
+        //also it will print sum of odd numbers and sum of even numbers
 
         int [][] number2D={
                 {1,2,3,4},
                 {5,6,7,20},
-                {8,9,10,11}
+                {8,9,10,11,15}
         };
 
         int countOdd=0;
@@ -19,17 +19,21 @@ public class c7_CountSumOfEven {
         int sumEven=0;
         int sumOdd=0;
 
-        for (int [] each1d : number2D){
+        for (int [] each1d : number2D){ // 0 1 2 each1d
 
-            for (int eachNumber : each1d){
+            for (int eachNumber : each1d){//0123  0123  01234
                 if (eachNumber %2 == 0 ){
-                    countEven++;
-                    sumEven += eachNumber;
+                    countEven++;//1234.. this will give you total count of even numbers
+                    sumEven += eachNumber; //2+ 4 +6
                     System.out.println(eachNumber + " is even number");
+                    //System.out.println(sumEven);
+                    //System.out.println(countEven);
                 }else { //odd numbers
-                    countOdd++;
-                    sumOdd += eachNumber;
+                    countOdd++;//12345... this will give you total count of odd numbers
+                    sumOdd += eachNumber;  // 1 +  3 +5  ...
                     System.out.println(eachNumber + " is odd number");
+                    //System.out.println(sumOdd);
+                    //System.out.println(countOdd);
 
                 }
 
@@ -38,8 +42,6 @@ public class c7_CountSumOfEven {
 
         System.out.println("Count of even numbers : "+ countEven + " sum of even numbers " + sumEven);
         System.out.println("Count of odd numbers : "+ countOdd + " sum of odd numbers " + sumOdd);
-
-
 
     }
 }
