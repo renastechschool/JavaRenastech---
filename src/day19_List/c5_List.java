4a0b0f19ae9235d0f3a66d242e6ed57e74a14d76
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-09-28 20:53:27 -0400

    list

---
+++ src/day19_List/c5_List.java
@@ -0,0 +1,32 @@
+package day19_List;
+
+import java.util.ArrayList;
+
+public class c5_List {
+
+    //Data Structure:
+    //	1. Array
+    //	2. Collection ==> only accepts objects
+    //	3. Map ==>  only accepts objects
+
+    // ArrayList<DataType>  listName = new ArrayList<DataType>();
+    //DataType can not be primitive
+    //they need to be wrapper class ,object
+
+    //ArrayList: part of Collections
+    //			does not support primitives, only support none primitives
+    //			size is dynamic, automatically adjusted
+    //			has index numbers
+
+    //what is the differences between array and array list
+    //1.size  : array size is fixed and ; arrraylist size is dynamic
+    //2.array accepts primitives arraylist doesnt accept primitives
+    //3.array is data structure and arraylist is part of collection data structure
+
+    public static void main(String[] args) {
+        // ArrayList<DataType>  listName = new ArrayList<DataType>();
+           //ArrayList <int>        list1    =new ArrayList<int>(); collections doesnt accept primitives
+           ArrayList <Integer>        list2    =new ArrayList<Integer>();
+           //size of list 2 is 0 for now and this size is flexible
+    }
+}
