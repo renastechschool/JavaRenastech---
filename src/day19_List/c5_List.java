60befde06109ff150931828ca412f5aaa6c4e496
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-09-28 21:24:33 -0400

    list

--- src/day19_List/c5_List.java
+++ src/day19_List/c5_List.java
@@ -28,5 +28,55 @@ public class c5_List {
            //ArrayList <int>        list1    =new ArrayList<int>(); collections doesnt accept primitives
            ArrayList <Integer>        list2    =new ArrayList<Integer>();
            //size of list 2 is 0 for now and this size is flexible
+
+        //add() this method will add data to your list
+        list2.add(25); //size 1 index 0
+        list2.add(55); //size 2 index 1
+        list2.add(100); //size 3 index 2
+        list2.add(200); //size 4 index 3
+
+        System.out.println(list2);
+
+        int [] numbers=new int[10]; //size is fixed 10
+
+        //get() that will give you specific number from list
+        Integer numIndex1=list2.get(1);
+        System.out.println("numIndex1 = " + numIndex1);
+        System.out.println(list2.get(3));
+       // System.out.println(list2.get(4));//IndexOutOfBoundsException
+
+        //list2.get(0); // will return 25 as wrapper object
+
+        int number0=list2.get(0);
+        //unboxing will hep us to store wrapper objectt to primitive data type
+        System.out.println(number0);
+
+
+        ArrayList<String> nameList=new ArrayList<>(); //size 0
+        nameList.add("Meral");
+        nameList.add("Recep");
+        nameList.add("Musab");
+        nameList.add("Bawer");//size 4 index 3
+
+        System.out.println(nameList);
+
+
+        //size() will give us size of arraylist
+        System.out.println("Size of namelist is "+ nameList.size());
+        System.out.println("Names are : ");
+        for (int i=0 ; i <= nameList.size() -1 ; i ++){
+            System.out.println("nameList.get("+i+") = " + nameList.get(i));
+                                                        //namelist[i]
+        }
+
+        nameList.add("ridvan"); //size 5 index 4
+        nameList.add("Jessica");
+
+        System.out.println(nameList.size());
     }
 }
+
+
+//add
+//get
+//size
