58dfef150c47b56f3cbc3e5acddbfdbfa70ab0b0
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-10-05 20:27:28 -0400

    custom class

--- src/day22_CustomClasses_StaticVariables/AnimalsObjects.java
+++ src/day22_CustomClasses_StaticVariables/AnimalsObjects.java
@@ -1,5 +1,10 @@
 package day22_CustomClasses_StaticVariables;
 
+import MyUtil.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AnimalsObjects {
 
     public static void main(String[] args) {
@@ -13,6 +18,26 @@ public class AnimalsObjects {
 
         animal1.eating("bread");
         animal1.eating("fish");
+
+        Animals animal2=new Animals();
+        animal2.setAnimalInfo("bird",3,"blue","bird1");
+
+        Animals animal3=new Animals();
+        animal3.setAnimalInfo("dog",5,"Black","dog1");
+
+        //store animal object in the arraylist
+        //data type of arraylist will be animal objects
+        ArrayList<Animals> animalList=new ArrayList<>();
+        animalList.addAll(Arrays.asList(animal1,animal2,animal3));
+
+        String food1="banana";
+        for (Animals each :animalList){
+            util.stars();
+            System.out.println(each.name);
+
+            System.out.println(each);//since we dont have to string this will return you hashcode
+            each.eating(food1);
+        }
 
     }
 }
