7b5d6b83b76c77268726ecf4a4da44165a4b8968
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-10-11 21:00:33 -0400

    access modifiers

---
+++ src/day24_AccessModifiers/EncapsulationIntro.java
@@ -0,0 +1,25 @@
+package day24_AccessModifiers;
+
+public class EncapsulationIntro {
+ /*
+     OOP PRINCIPLES : object oriented programing language
+•Encapsulation :(Data Hiding)
+•Inheritance   :
+•Abstraction   :
+•Polymorhism   :
+
+OOP  will combine group of realated variables and functıonus to unit (object).
+     */
+
+    /*
+Encapsulation in Java is a mechanism of wrapping the data (variables) and code acting on the data (methods) together as a single unit.
+In encapsulation, the variables of a class will be hidden from other classes, and can be accessed only through the methods of their current class.
+Therefore, it is also known as data hiding.
+
+To achieve encapsulation in Java −
+1.Declare the variables of a class as private.
+2.Provide public setter and getter methods to modify and view the variables values.
+
+     */
+
+}
