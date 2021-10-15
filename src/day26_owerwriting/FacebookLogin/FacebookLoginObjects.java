480d66de701d9e9682b3ade94a8a8cbb0b067b64
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-10-14 21:24:32 -0400

    overwriting

--- src/day26_owerwriting/FacebookLogin/FacebookLoginObjects.java
+++ src/day26_owerwriting/FacebookLogin/FacebookLoginObjects.java
@@ -1,5 +1,7 @@
 package day26_owerwriting.FacebookLogin;
 
+import MyUtil.util;
+
 public class FacebookLoginObjects {
 
     public static void main(String[] args) {
@@ -11,6 +13,35 @@ public class FacebookLoginObjects {
 
         System.out.println("obj1.getUsername() = " + obj1.getUsername());
         System.out.println("obj1.getPassword() = " + obj1.getPassword());
+
+        util.stars();
+
+        FacebookLoginChrome chrome=new FacebookLoginChrome();
+        chrome.openBrowser();
+        chrome.goToUrl();
+        chrome.passingCredentials("firas", "java");
+        System.out.println("chrome.getUsername() = " + chrome.getUsername());
+        System.out.println("chrome.getPassword() = " + chrome.getPassword());
+        chrome.closeBrowser();
+
+        util.stars();
+        FacebookLoginFirefox firefox=new FacebookLoginFirefox();
+        firefox.openBrowser();
+        firefox.goToUrl();
+        firefox.passingCredentials("ridvan " ," password123");
+        System.out.println("firefox.getUsername() = " + firefox.getUsername());
+        System.out.println("firefox.getPassword() = " + firefox.getPassword());
+        firefox.closeBrowser();
+
+        util.stars();
+
+        FacebookLoginEdge edge=new FacebookLoginEdge();
+        edge.openBrowser();
+        edge.goToUrl();
+        edge.passingCredentials("bahast " ," hello world");
+        System.out.println("edge.getUsername() = " + edge.getUsername());
+        System.out.println("edge.getPassword() = " + edge.getPassword());
+        edge.closeBrowser();
 
     }
 }
