160be45ad1c007fe480d94d5df8d970250bdd5d0
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-10-19 20:05:38 -0400

    abstraction

--- src/day28_Abstraction/ShapeObject.java
+++ src/day28_Abstraction/ShapeObject.java
@@ -8,5 +8,14 @@ public class ShapeObject {
         //area 5 * 7 =35
         obj.shapeName();
         obj.shapeArea();
+
+        c1_Rectangle obj2 = new c1_Rectangle(3,8);
+        obj2.shapeName();
+        obj2.shapeArea();
+
+        c2_Square obj3=new c2_Square(5);
+        //area of square is length * length 25
+        obj3.shapeName();
+        obj3.shapeArea();
     }
 }
